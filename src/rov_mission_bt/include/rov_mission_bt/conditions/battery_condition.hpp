4224b9912b9208044bd90cef14fe6027ee348af5
abubakaraliyubadawi <abubakb@stud.ntnu.no>
2025-02-07 23:00:53 +0100

    behaviour modification

---
+++ src/rov_mission_bt/include/rov_mission_bt/conditions/battery_condition.hpp
@@ -0,0 +1,27 @@
+#ifndef BATTERY_CONDITION_HPP
+#define BATTERY_CONDITION_HPP
+
+#include <behaviortree_cpp_v3/condition_node.h>
+#include <rclcpp/rclcpp.hpp>
+#include <mundus_mir_msgs/msg/battery_status.hpp>
+#include <mundus_mir_msgs/msg/return_recommendation.hpp>
+
+class CheckBatteryLevel : public BT::ConditionNode {
+private:
+    rclcpp::Node::SharedPtr node_;
+    rclcpp::Subscription<mundus_mir_msgs::msg::BatteryStatus>::SharedPtr battery_sub_;
+    rclcpp::Subscription<mundus_mir_msgs::msg::ReturnRecommendation>::SharedPtr return_sub_;
+    bool should_return_;
+    double battery_level_;
+
+public:
+    CheckBatteryLevel(const std::string& name, const BT::NodeConfiguration& config);
+    static BT::PortsList providedPorts();
+    BT::NodeStatus tick() override;
+
+private:
+    void batteryCallback(const mundus_mir_msgs::msg::BatteryStatus::SharedPtr msg);
+    void returnCallback(const mundus_mir_msgs::msg::ReturnRecommendation::SharedPtr msg);
+};
+
+#endif // BATTERY_CONDITION_HPP
