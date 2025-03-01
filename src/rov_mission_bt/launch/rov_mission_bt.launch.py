4149d39b84ddaaae04bf5f89fb706460e53ee650
abubakaraliyubadawi <abubakb@stud.ntnu.no>
2025-03-01 23:08:00 +0100

    switched to pipeline simulator

--- src/rov_mission_bt/launch/rov_mission_bt.launch.py
+++ src/rov_mission_bt/launch/rov_mission_bt.launch.py
@@ -23,46 +23,46 @@ def generate_launch_description():
         }]
     )
 
-    # battery_management_node = Node(
-    #     package='gz_battery_management',
-    #     executable='battery_management',
-    #     name='battery_management',
-    #     parameters=[{
-    #         'window_size': 300.0,
-    #         'safety_margin': 30.0,
-    #         'update_frequency': 0.1,
-    #         'min_samples_for_average': 10
-    #     }],
-    #     output='screen'
-    # )
+    battery_management_node = Node(
+        package='gz_battery_management',
+        executable='battery_management',
+        name='battery_management',
+        parameters=[{
+            'window_size': 300.0,
+            'safety_margin': 30.0,
+            'update_frequency': 0.1,
+            'min_samples_for_average': 10
+        }],
+        output='screen'
+    )
 
-    # dock_distance_node = Node(
-    #     package='gz_battery_management',
-    #     executable='dock_distance_calc',
-    #     name='dock_distance_calc',
-    #     output='screen'
-    # )
+    dock_distance_node = Node(
+        package='gz_battery_management',
+        executable='dock_distance_calc',
+        name='dock_distance_calc',
+        output='screen'
+    )
 
-    # battery_node = Node(
-    #     package='gz_battery',
-    #     executable='gz_battery_node',
-    #     name='gz_battery',
-    #     output='screen'
-    # )
+    battery_node = Node(
+        package='gz_battery',
+        executable='gz_battery_node',
+        name='gz_battery',
+        output='screen'
+    )
 
-    # sonar_node = Node(
-    #     package='gz_sonar',
-    #     executable='gz_sonar',
-    #     name='gz_sonar',
-    #     output='screen'
-    # )
+    sonar_node = Node(
+        package='gz_sonar',
+        executable='gz_sonar',
+        name='gz_sonar',
+        output='screen'
+    )
 
     ld = LaunchDescription([
-        rov_mission_node
-        # battery_management_node,
-        # dock_distance_node,
-        # battery_node,
-        # sonar_node
+        rov_mission_node,
+        battery_management_node,
+        dock_distance_node,
+        battery_node,
+        sonar_node
     ])
     
     return ld
